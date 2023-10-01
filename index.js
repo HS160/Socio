dedbf074c075a97caca2892568e424940b877b2d
Harsh Solanki <134034742+HS160@users.noreply.github.com>
2023-10-01 21:58:07 +0530

    index.js

--- index.js
+++ index.js
@@ -67,67 +67,67 @@ theme.addEventListener('click', openThemeModal);
 
 
 //================fonts================
-// const removeSizeSelector =() => {
-//     fontSizes.forEach(size =>{
-//         size.classList.remove('active');
-//     })
-// }
+ const removeSizeSelector =() => {
+     fontSizes.forEach(size =>{
+         size.classList.remove('active');
+     })
+ }
 
-// fontSizes.forEach(size =>{
+    fontSizes.forEach(size =>{
     
 
-//     size.addEventListener('click',() =>{
-//         removeSizeSelector();
-//         let fontSize;
-//         size.classList.toggle('active');
-//         if(size.classList.contains('font-size-1'))
-//     {
-//         fontSize ='10px';
-//         root.style.setProperty('----sticky-top-left', '5.4rem');
-//         root.style.setProperty('----sticky-top-right', '5.4rem');
-//     }else if(size.classList.contains('font-size-2')){
-//         fontSize ='13px';
-//         root.style.setProperty('----sticky-top-left', '5.4rem');
-//         root.style.setProperty('----sticky-top-right', '-7rem');
-//     }else if(size.classList.contains('font-size-3')){
-//         fontSize ='16px';
-//         root.style.setProperty('----sticky-top-left', '-2rem');
-//         root.style.setProperty('----sticky-top-right', '-17rem');
-//     }else if(size.classList.contains('font-size-4')){
-//         fontSize ='19px';
-//         root.style.setProperty('----sticky-top-left', '-5rem');
-//         root.style.setProperty('----sticky-top-right', '-25rem');
-//     }else if(size.classList.contains('font-size-5')){
-//         fontSize ='22px';
-//         root.style.setProperty('----sticky-top-left', '-12rem');
-//         root.style.setProperty('----sticky-top-right', '-35rem');
-//     }
+     size.addEventListener('click',() =>{
+         removeSizeSelector();
+         let fontSize;
+         size.classList.toggle('active');
+         if(size.classList.contains('font-size-1'))
+     {
+         fontSize ='10px';
+         root.style.setProperty('----sticky-top-left', '5.4rem');
+         root.style.setProperty('----sticky-top-right', '5.4rem');
+     }else if(size.classList.contains('font-size-2')){
+         fontSize ='13px';
+         root.style.setProperty('----sticky-top-left', '5.4rem');
+         root.style.setProperty('----sticky-top-right', '-7rem');
+     }else if(size.classList.contains('font-size-3')){
+         fontSize ='16px';
+         root.style.setProperty('----sticky-top-left', '-2rem');
+         root.style.setProperty('----sticky-top-right', '-17rem');
+     }else if(size.classList.contains('font-size-4')){
+         fontSize ='19px';
+         root.style.setProperty('----sticky-top-left', '-5rem');
+         root.style.setProperty('----sticky-top-right', '-25rem');
+     }else if(size.classList.contains('font-size-5')){
+         fontSize ='22px';
+         root.style.setProperty('----sticky-top-left', '-12rem');
+         root.style.setProperty('----sticky-top-right', '-35rem');
+     }
 
-//     document.querySelector('html').style.fontSize = fontSize;
-//     })
-// })
+     document.querySelector('html').style.fontSize = fontSize;
+     })
+ })
 
 // //primary color
-// colorPalette.forEach(color => {
-//     color.addEventListener('click', () => {
-//         let primaryHue;
+    colorPalette.forEach(color => {
+     color.addEventListener('click', () => {
+         let primaryHue;
 
-//         if(color.classList.contains('color-1')){
-//             primaryHue = 252;
-//         }
-//         else if(color.classList.contains('color-2')){
-//             primaryHue = 52;
-//         }
-//         else if(color.classList.contains('color-3')){
-//             primaryHue = 352;
-//         }
-//         else if(color.classList.contains('color-4')){
-//             primaryHue = 152;
+         if(color.classList.contains('color-1')){
+             primaryHue = 252;
+         }
+         else if(color.classList.contains('color-2')){
+             primaryHue = 52;
+         }
+         else if(color.classList.contains('color-3')){
+             primaryHue = 352;
+         }
+         else if(color.classList.contains('color-4')){
+             primaryHue = 152;
 
-//         }else if(color.classList.contains('color-5')){
-//             primaryHue = 202;
-//         }        
-//         color.classList.add('active');
-//         root.style.setProperty('--primary-color-hue',primaryHue);
-//     })
-// })
+         }else if(color.classList.contains('color-5')){
+             primaryHue = 202;
+         }        
+         color.classList.add('active');
+         root.style.setProperty('--primary-color-hue',primaryHue);
+     })
+ })
